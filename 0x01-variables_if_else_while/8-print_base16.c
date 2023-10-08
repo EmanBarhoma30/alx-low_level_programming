461844586ccae6528a8a6c94b4956875854a36cf
emanbarhoma30 <emanahmedb92@gmail.com>
2023-10-08 13:35:17 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9 and letters between a to f.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    i = 48;
+    while (i < 58)
+    {
+        putchar(i);
+        i++;
+    }
+    i = 97;
+    while (i < 103)
+    {
+        putchar(i);
+        i++;
+    }
+    putchar('\n');
+    return (0);
+}
