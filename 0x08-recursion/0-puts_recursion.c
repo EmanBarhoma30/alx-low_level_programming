f3049c10fb5a4f8da40ca3891edff07ddf756a78
emanbarhoma30 <emanahmedb92@gmail.com>
2023-10-25 14:10:05 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: The string to print
+ */
+void _puts_recursion(char *s) {
+    if (*s == '\0') {
+        _putchar('\n'); /* Print a new line when the end of the string is reached */
+        return;
+    }
+
+    _putchar(*s); /* Print the current character */
+    _puts_recursion(s + 1); /* Move to the next character and call the function recursively */
+}
