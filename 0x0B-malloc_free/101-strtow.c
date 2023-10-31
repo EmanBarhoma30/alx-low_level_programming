1dc68ec6a8a1c7f3d334e60566b795d2f211f89d
emanbarhoma30 <emanahmedb92@gmail.com>
2023-10-30 17:01:49 -0700

    6. I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,66 +1,37 @@
 #include <stdlib.h>
 
+char **strtow(char *);
+
 /**
- * strtow - Splits a string into an array of words.
- * @str: The input string.
+ * print_tab - Prints an array of string
  *
- * Return: A pointer to an array of strings.
+ * @tab: The array to print
  */
-char **strtow(char *str)
+void print_tab(char **tab)
 {
-	int i, j, k, wordCount = 0;
-	char **words;
+	int i;
 
-	if (str == NULL || str[0] == '\0')
+	for (i = 0; tab[i] != NULL; ++i)
 	{
-		words = malloc(1 * sizeof(char *));
-		if (words == NULL)
-			return (NULL);
-		words[0] = NULL;
-		return (words);
+		printf("%s\n", tab[i]);
 	}
+}
 
-	for (i = 0; str[i]; i++)
+/**
+ * main - check the code .
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char **tab;
+
+	tab = strtow("                ");
+	if (tab == NULL)
 	{
-		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-			wordCount++;
+		printf("Failed\n");
+		return (1);
 	}
-
-	words = malloc((wordCount + 1) * sizeof(char *));
-	if (words == NULL)
-		return (NULL);
-
-	for (i = 0, j = 0; j < wordCount;)
-	{
-		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-		{
-			int wordLength = 0;
-
-			while (str[i + wordLength] && str[i + wordLength] != ' ')
-				wordLength++;
-
-			words[j] = malloc((wordLength + 1) * sizeof(char));
-			if (words[j] == NULL)
-			{
-				while (j >= 0)
-				{
-					free(words[j]);
-					j--;
-				}
-				free(words);
-				return (NULL);
-			}
-
-			for (k = 0; k < wordLength; k++)
-				words[j][k] = str[i + k];
-			words[j][k] = '\0';
-			j++;
-			i += wordLength;
-		}
-		else
-			i++;
-	}
-
-	words[j] = NULL;
-	return (words);
+	print_tab(tab);
+	return (0);
 }
