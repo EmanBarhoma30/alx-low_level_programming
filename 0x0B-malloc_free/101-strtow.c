2017a1e72f0fd4e8590b826d6c4879f0234dccf5
emanbarhoma30 <emanahmedb92@gmail.com>
2023-10-31 13:12:53 -0700

    try

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,7 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char **strtow(char *);
+/**
+ * wordCount - helper function to count words in a string
+ * @str: the string to process
+ *
+ * Return: number of words in string
+ */
+int wordCount(char *str)
+{
+	int i, words = 0;
+
+	for (i = 0; str[i]; i++)
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+			words++;
+	return (words);
+}
+
+/**
+ * strtow - splits strings into words
+ * @str: the string to split
+ *
+ * Return: a pointer to an array of strings (words), NULL if failure
+ */
+char **strtow(char *str)
+{
+	int i = 0, j = 0, k = 0;
+	char **words;
+
+	if (str == NULL || *str == '\0' || wordCount(str) == 0)
+		return (NULL);
+
+	words = malloc((wordCount(str) + 1) * sizeof(char *));
+	if (words == NULL)
+		return (NULL);
+
+	for (; str[i] && j < wordCount(str); )
+	{
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+		{
+			int wordLength = 0;
+
+			while (str[i + wordLength] && str[i + wordLength] != ' ')
+				wordLength++;
+
+			words[j] = malloc((wordLength + 1) * sizeof(char));
+			if (words[j] == NULL)
+				return (NULL);
+
+			for (k = 0; k < wordLength; k++)
+				words[j][k] = str[i + k];
+			words[j][k] = '\0';
+			j++;
+			i += wordLength;
+		}
+		else
+			i++;
+	}
+	words[j] = NULL;
+	return (words);
+}
 
 /**
  * print_tab - Prints an array of string
@@ -19,7 +77,7 @@ void print_tab(char **tab)
 }
 
 /**
- * main - check the code .
+ * main - check the code
  *
  * Return: Always 0.
  */
