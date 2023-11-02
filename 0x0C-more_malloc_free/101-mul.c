9048cab30fe615a1885454bc63a4558235a875e2
emanbarhoma30 <emanahmedb92@gmail.com>
2023-11-02 13:44:45 -0700

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
- * is_positive_number - Check if a string contains only digits.
- * @s: The string to be evaluated.
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
- * Return: 1 if all characters are digits, 0 otherwise.
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-int is_positive_number(char *s)
+int is_digit(char *s)
 {
 	int i = 0;
 
@@ -21,82 +22,78 @@ int is_positive_number(char *s)
 }
 
 /**
- * multiply - Multiply two positive numbers represented as strings.
- * @s1: The first number.
- * @s2: The second number.
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * Return: A dynamically allocated string containing the product.
+ * Return: the length of the string
  */
-char *multiply(char *s1, char *s2)
+int _strlen(char *s)
 {
-	int len1 = 0, len2 = 0, i, j, carry = 0, digit1, digit2, result_len;
-	char *result;
+	int i = 0;
 
-	while (s1[len1])
-		len1++;
-	while (s2[len2])
-		len2++;
-
-	result_len = len1 + len2;
-	result = calloc(result_len + 1, sizeof(char));
-
-	if (!result)
+	while (s[i] != '\0')
 	{
-		return (NULL);
+		i++;
 	}
-
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		digit1 = s1[i] - '0';
-		carry = 0;
-
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			digit2 = s2[j] - '0';
-			carry += result[i + j + 1] + (digit1 * digit2);
-			result[i + j + 1] = carry % 10;
-			carry /= 10;
-		}
-
-		if (carry > 0)
-			result[i + j + 1] += carry;
-	}
-
-	return (result);
+	return (i);
 }
 
 /**
- * main - Multiplies two positive numbers passed as arguments.
- * @argc: The number of command-line arguments.
- * @argv: An array of command-line argument strings.
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: 0 on success, 98 on error.
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	char *s1, *s2, *product;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	if (argc != 3 || !is_positive_number(argv[1]) || !is_positive_number(argv[2]))
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		printf("Error\n");
-		return (98);
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-
-	s1 = argv[1];
-	s2 = argv[2];
-
-	product = multiply(s1, s2);
-
-	if (!product)
+	for (i = 0; i < len - 1; i++)
 	{
-		printf("Error\n");
-		return (98);
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
-
-	/* Print the product */
-	printf("%s\n", product);
-
-	free(product);
-
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
