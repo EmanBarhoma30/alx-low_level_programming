3beba61ebd05bae3d7f2a9e00434b4de22417949
emanbarhoma30 <emanahmedb92@gmail.com>
2023-11-06 11:45:04 -0800

    2. File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: This function prints the name of the file.
+ * main - prints the name of the file
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int ain(void)
 {
-    printf("%s\n", __FILE__);
-    return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
