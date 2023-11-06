a1c83f34f99031b3b55214bcb9d6e52f05acb3d7
emanbarhoma30 <emanahmedb92@gmail.com>
2023-11-06 12:23:36 -0800

    2. A dog will teach you unconditional love. If you can have that in your life, things won't be too bad

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,31 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
 
 /**
- * struct dog - Represents basic information about a dog
- * @name: The name of the dog
- * @age: The age of the dog
- * @owner: The owner of the dog
- *
- * Description: This structure defines basic information about a dog,
- * including its name, age, and owner.
- */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-
-/**
- * print_dog - Prints information about a dog
- * @d: Pointer to the dog structure to be printed
+ * print_dog - Prints the information of a struct dog
+ * @d: Pointer to the struct dog to print
  */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
-	{
-		printf("Name: %s\n", d->name ? d->name : "(nil)");
-		printf("Age: %.1f\n", d->age);
-		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
-	}
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
