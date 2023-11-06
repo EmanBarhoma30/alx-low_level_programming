11608d1b3ae97a460cebebb262aab79a77168328
emanbarhoma30 <emanahmedb92@gmail.com>
2023-11-06 11:49:19 -0800

    done

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: This function prints the name of the current source file.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     printf("%s\n", __FILE__);
