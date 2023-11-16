7994be04f7d967657aecf5693bf11e5a58c38879
emanbarhoma30 <emanahmedb92@gmail.com>
2023-11-16 13:20:48 -0800

    advanced done

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,15 @@
-section .data
-    format db "Hello, Holberton", 0
+global main
+extern printf
 
-section .text
-    global main
-    extern printf
-
+	section .text
 main:
-    mov edi, format
-    xor eax, eax
-    call printf
-    mov eax, 0
-    ret
+	push rbp
+	mov rdi, format
+	mov rsi, message
+	mov rax, 0
+	call printf
+	pop rbp
+	mov rax, 0
+	ret
+message: db "Hello, Holberton", 0
+format:	db "%s", 10, 0
